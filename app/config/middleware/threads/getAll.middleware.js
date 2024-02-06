c062718139ad3aff9d036be7d95a07ec69a47c94
Naufal Mahadian <naupl.ramadhan@gmail.com>
2024-02-06 20:12:39 +0700

    fixed some lines of codes in thread.controller.js and comment.controller.js

--- app/config/middleware/threads/getAll.middleware.js
+++ app/config/middleware/threads/getAll.middleware.js
@@ -1,30 +1,32 @@
 const models = require('../../../src/models/index')
 const Like = models.Like
+const Comment = models.Comment
 
 async function getAllThreadData() {
-  try {
-    const threads = await models.Thread.find({})
+    try {
+        const threads = await models
+            .Thread
+            .find({})
 
-    const threadData = await Promise.all(threads.map(async (thread) => {
-      const likeCount = await Like.countDocuments({
-        threadID: thread._id
-      })
+        const threadData = await Promise.all(threads.map(async(thread) => {
+            const likeCount = await Like.countDocuments({threadID: thread._id})
+            const commentCount = await Comment.countDocuments({threadID: thread._id})
 
-      // return data
-      return {
-        _id: thread._id,
-        title: thread.title,
-        text: thread.text,
-        userID: thread.userID,
-        Likes: likeCount,
-        Comments: thread.comments.length, 
-      }
-    }))
+            // return data
+            return {
+                _id: thread._id,
+                title: thread.title,
+                text: thread.text,
+                userID: thread.userID,
+                Likes: likeCount,
+                Comments: commentCount
+            }
+        }))
 
-    return threadData
-  } catch (err) {
-    throw err
-  }
+        return threadData
+    } catch (err) {
+        throw err
+    }
 }
 
 module.exports = getAllThreadData
